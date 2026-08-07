1d0d1c25b2de6b703ead727a9e3d9b6456b5f1b5
helenksite <udehelen024@gmail.com>
2026-08-07 14:30:21 +0100

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import HelenKStore from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <HelenKStore />
+  </React.StrictMode>
+);
