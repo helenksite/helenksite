9a1be338e1f6b2362ef50a93d72f769f1840bf1d
helenksite <udehelen024@gmail.com>
2026-08-07 14:21:21 +0100

    Delete src/main.jsx

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import HelenKStore from "./App.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <HelenKStore />
-  </React.StrictMode>
-);
